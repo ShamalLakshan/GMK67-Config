cfdef54efb3a77407d2d05f78343d8224187fa60
ShamalLakshan <shamallakshan00600@gmail.com>
2024-07-19 11:08:56 +0530

    system daemon reload

--- autostartup.sh
+++ autostartup.sh
@@ -2,6 +2,8 @@
 
 nano /etc/systemd/system/gmk67.service
 
+cp ./gmk67fnrow.sh /opt/
+
 cat << EOF > /etc/systemd/system/gmk67.service
 [Unit]
 Description=Enable Fn Row of GMK67
@@ -11,4 +13,6 @@ ExecStart=/opt/gmk67fnrow.sh
 [Install]
 WantedBy=default.target
 EOF
+
+systemctl daemon-reload
 
