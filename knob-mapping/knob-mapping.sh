4d53f3b707f36aca9c298e7e8f5cedf8d3c506a4
ShamalLakshan <shamallakshan00600@gmail.com>
2024-07-19 18:57:49 +0530

    basic script

---
+++ knob-mapping/knob-mapping.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.settings-daemon.plugins.media-keys play "['AudioMute']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys volume-down "['AudioLowerVolume']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys volume-up "['AudioRaiseVolume']"
